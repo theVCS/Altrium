349c852fe990a8920eada40a6f33522edb3f6bfe
Prince Mishra <69673655+theVCS@users.noreply.github.com>
2023-01-22 19:27:48 +0530

    added task 3

---
+++ task3/script.js
@@ -0,0 +1,213 @@
+const grpType = ["bar", "line", "scatter", "doughnut", "polarArea", "radar"];
+const sz = grpType.length;
+let index = 0;
+let flag = true;
+let currId = "dragGrp";
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+let grid = GridStack.init({
+  cellHeight: 70,
+  acceptWidgets: true,
+  dragIn: ".newWidget",
+  dragInOptions: { appendTo: "body", helper: "clone" },
+  removable: "#trash",
+});
+
+async function getDashBoardData() {
+  return await $.post(
+    "http://127.0.0.1:8080/getData",
+    function (data, status, jqXHR) {
+      return data;
+    }
+  );
+}
+
+async function getData(length) {
+  return await $.post(
+    "http://127.0.0.1:8080",
+    { length: length },
+    function (data, status, jqXHR) {
+      return data;
+    }
+  );
+}
+
+async function addDataToGraph(id, type) {
+  let point = await getData(30);
+  const x = point.x;
+  const y = point.y;
+
+  let chartStatus = Chart.getChart(id);
+  if (chartStatus != undefined) {
+    chartStatus.destroy();
+  }
+
+  const ctx = document.getElementById(id);
+  const chart = new Chart(ctx, {
+    type: type,
+    data: {
+      labels: x,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: y,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+}
+
+async function InitDashBoard() {
+  const data = await getDashBoardData();
+  grid.load(data.gridData);
+
+  for (item of data.metaData) {
+    if(item.type=="table")continue;
+    addDataToGraph(item.id, item.type);
+  }
+
+  addDataToGraph(currId, grpType[index]);
+}
+
+function addNested(type) {
+  const newId = makeid(20);
+  const content = `<canvas id="${newId}"></canvas>`;
+  grid.addWidget({ x: 0, y: 100, w: 4, h: 3, content: content });
+  addDataToGraph(newId, type);
+  flag = false;
+}
+
+grid.on("added removed change", function (e, items) {
+  if (e.type == "added" && flag) {
+    const ele = document.getElementById(currId);
+    const newId = makeid(20);
+    ele.id = newId;
+    const dragEle = document.getElementById(currId);
+    if (dragEle != null) addDataToGraph(currId, grpType[index]);
+    currId = newId;
+    index = (index + 1) % sz;
+    addDataToGraph(currId, grpType[index]);
+  }
+
+  flag = true;
+
+  let str = "";
+  items.forEach(function (item) {
+    str += " (x,y)=" + item.x + "," + item.y;
+  });
+  console.log(e.type + " " + items.length + " items:" + str);
+});
+
+async function saveConfiguration() {
+  var items = [];
+
+  for (const ele of $(".grid-stack-item")) {
+    let canvas = ele
+      .querySelector(".grid-stack-item-content")
+      .querySelector("canvas");
+
+    if (canvas === null) {
+      const content = ele
+        .querySelector(".grid-stack-item-content")
+        .querySelector("table");
+
+      items.push({
+        x: ele.getAttribute("gs-x"),
+        y: ele.getAttribute("gs-y"),
+        w: ele.getAttribute("gs-w"),
+        h: ele.getAttribute("gs-h"),
+        content: content,
+        id: "none",
+        type: "table",
+      });
+    } else {
+      const id = canvas.id;
+      let graph = Chart.getChart(id);
+      if (id == currId) continue;
+      const type = graph.config._config.type;
+
+      items.push({
+        x: ele.getAttribute("gs-x"),
+        y: ele.getAttribute("gs-y"),
+        w: ele.getAttribute("gs-w"),
+        h: ele.getAttribute("gs-h"),
+        content: `<canvas id="${id}"></canvas>`,
+        id: id,
+        type: type,
+      });
+    }
+  }
+  $.post(
+    "http://127.0.0.1:8080/saveConfig",
+    { data: items },
+    function (data, status, jqXHR) {
+      alert("configuration saved to database");
+    }
+  );
+}
+
+function compact() {
+  grid.compact();
+}
+
+function addTable() {
+  const constriant = 10;
+  const row = prompt("enter row count");
+  const col = parseInt(prompt("enter col count"));
+  if (
+    !parseInt(row) ||
+    !parseInt(col) ||
+    row > constriant ||
+    col > constriant
+  ) {
+    alert(`please enter an integer and must be less than ${constriant}`);
+  } else {
+    const newId = makeid(20);
+    let content = `<table class="table">
+    <thead>
+      <tr>`;
+
+    for (let index = 0; index < col; index++) {
+      content += `<th scope="col">col${index + 1}</th>`;
+    }
+
+    content += `</tr>
+    </thead>
+    <tbody>`;
+
+    for (let index = 0; index < row; index++) {
+      content += `<tr>`;
+      for (let index2 = 0; index2 < col; index2++) {
+        content += `<th scope="row">data${index2 + 1}</th>`;
+      }
+      content += `</tr>`;
+    }
+
+    content += `</tbody></table>`;
+    grid.addWidget({ x: 0, y: 100, w: 4, h: 3, content: content });
+    addDataToGraph(newId, type);
+    flag = false;
+  }
+}
+
+InitDashBoard();
---
+++ task3/server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const Schema = mongoose.Schema;
+mongoose.set('strictQuery', true);
+
+const GraphSchema = new Schema({
+  x: String,
+  y: String,
+  w: String,
+  h: String,
+  id: String,
+  content: String,
+  type: String,
+});
+
+const grpModel = mongoose.model('graph', GraphSchema)
+
+const username = process.env.USER;
+const password = process.env.PASSWORD;
+const adminPassword = encodeURIComponent(password);
+const database = "userDashBoard";
+const coll = "graphCollection";
+const uri = `mongodb+srv://${username}:${adminPassword}@cluster1.sjhpbbx.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri).then(() => console.log("Connected!"));
+
+
+app.listen(8080, function () {
+  console.log("callback function");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
+
+app.post("/", (req, res) => {
+  const len = req.body.length;
+  const y = Array.from({ length: len }, () => Math.floor(Math.random() * 40));
+  let x = new Array();
+
+  for (let index = 1; index <= len; index++) x.push(index);
+  
+  res.send({ x: x, y: y });
+});
+
+app.post("/getData", async (req, res) => {
+  const data = await grpModel.find();
+  let metaData = [];
+  let gridData = [];
+
+  for (const item of data) {
+    gridData.push({
+      x: item.x,
+      y: item.y,
+      w: item.w,
+      h: item.h,
+      content: item.content,
+    });
+
+    metaData.push({
+      id: item.id,
+      type: item.type,
+    });
+  }
+
+  res.send({ gridData: gridData, metaData: metaData });
+});
+
+app.post("/saveConfig", async (req, res) => {
+  const data = req.body.data;
+
+  let r = "";
+  r = await grpModel.deleteMany({});
+
+  if(data!=undefined)
+  for(const d of data)
+  {
+    r = new grpModel(d);
+    let result = await r.save();
+  }
+
+  res.send({ sccess: true });
+});
